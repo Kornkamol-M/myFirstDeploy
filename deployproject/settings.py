171fa666ea1adec86f6fe843f3482fb1aff00224
Kornkamol-M <85493283+Kornkamol-M@users.noreply.github.com>
2021-07-07 20:21:51 +0700

    config database at settings.py

--- deployproject/settings.py
+++ deployproject/settings.py
@@ -76,10 +76,22 @@ WSGI_APPLICATION = 'deployproject.wsgi.application'
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
 DATABASES = {
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': BASE_DIR / 'db.sqlite3',
+    # }
+
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'deipl4aeicaolg',
+        'USER': 'egwjgktixvsxir',
+        'PASSWORD': '210d20f68117796c46c8495bfe58ecccd0e8f176fee4f0653738599e4d99da3b',
+        'HOST': 'ec2-52-5-1-20.compute-1.amazonaws.com',
+        'PORT': '5432',
+        'CONN_MAX_AGE': 500,
+
     }
+
 }
 
 
